540337168fb9d299bdbfe6e0b0af31e84adb3ebe
Stephen Weiss <stephencweiss@gmail.com>
2018-11-13 21:12:19 -0800

    Updated MongoURI to local and added async/await for seed
    The async/await on the seed main function allows a much larger number of records to be created at once by leveraging the async/await method of allowing the event thread to proceed while not over flowing the memory heap tyring to keep everything in mind. Async/await, instead, uses pointers so that it can reconstruct the stack trace if necessary.

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb+srv://Spencer:Spencer@cluster0-0okak.mongodb.net/trailblazer'
-//const mongoUri = 'mongodb://localhost/trailblazer';
+// const mongoUri = 'mongodb+srv://Spencer:Spencer@cluster0-0okak.mongodb.net/trailblazer'
+const mongoUri = 'mongodb://localhost/trailblazer';
 
 mongoose.connect(mongoUri, {useNewUrlParser:true});
 const db = mongoose.connection;
