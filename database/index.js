814127813d6202cb96f6ee3cb6b6b3d389a2d30b
Stephen Weiss <stephencweiss@gmail.com>
2018-11-15 11:34:19 -0800

    Configred Mongo to stay up longer; added insertToMongo
    Mongo configurations in Index.js are intended to help keep the connection to the MongoDB alive longer for long-lived write queries.
    insertToMongo successfully writes to Mongo for small file sizes.

--- database/index.js
+++ database/index.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 // const mongoUri = 'mongodb+srv://Spencer:Spencer@cluster0-0okak.mongodb.net/trailblazer'
-const mongoUri = 'mongodb://localhost/trailblazer';
+const mongoUri = 'mongodb://localhost:27017/trailblazer';
 
-mongoose.connect(mongoUri, {useNewUrlParser:true});
+mongoose.connect(mongoUri, {
+  useNewUrlParser:true,
+  keepAlive: true,
+  poolSize: 1000,
+  connectTimeoutMS: 100000,
+  socketTimeoutMS: 200000
+});
 const db = mongoose.connection;
 module.exports = db;
 
--- database/seed.js
+++ database/seed.js
@@ -27,37 +27,65 @@ const generateRandomDescription = (productId) => {
 }
 
 const writeToTxt = function (recordCount, fileCount) {
+  console.time('CreateFiles')
   startingID = 0
   for (let i = 0; i < fileCount; i++) { 
-    console.log(`Starting file number --> ${i}`);
-    const file = fs.createWriteStream(path.join(__dirname,`sampleData_${i}.txt`))
+    // console.log(`Starting file number --> ${i}`);
+    // console.log(`Document's startingID is --> ${startingID}`);
+    const file = fs.createWriteStream(path.join(__dirname,`/sampleData/sampleData_${i}.txt`))
     for (let j = startingID; j < recordCount + startingID; j++) {
       let record = JSON.stringify(generateRandomDescription(j)).concat('\n');
       file.write(record)
     }
     file.end()
     startingID += recordCount
-    console.log(`Ending file number --> ${i}`);
+    // console.log(`Ending file number --> ${i}`);
   }
+  console.timeEnd('CreateFiles')
 }
 
-const readFromTxt = function (fileCount) {
-  for (let i = 0; i < fileCount; i++) {
-    const src = fs.createReadStream(path.join(__dirname,`sampleData_0.txt`))
+const readFromTxt = function (fileId) {
+  console.time('ReadFiles')
+  // for (let i = fileStart; i < fileEnd; i++) {
+    console.log(`Currently reading the file --> ${fileId}`);
+    const src = fs.createReadStream(path.join(__dirname,`sampleData/sampleData_${fileId}.txt`))
     let doc ='';
     src.on('data', (chunk) => {doc += chunk})
     src.on('end', () => {
       let recordsArray = doc.split('\n')//
+      console.log(`The length of the recordsArray is ${recordsArray.length}`);
       recordsArray.pop(); // eliminates the newline on the very last reccord
       recordsArray = recordsArray.map((rec) => {return JSON.parse(rec)});
+      // return recordsArray
       Description.create(recordsArray, (err) => {
         if (err) {console.log(`There's an error on insert --> `, err)}
       })
     })
+  console.timeEnd('ReadFiles')
+}
+
+const insertToMongo = async function (fileStart, fileEnd) {
+  console.time('WriteFilesToMongo')
+  for (let i = fileStart; i < fileEnd; i++) {
+    let recordsToCreate = await readFromTxt(i)
+    Description.create(recordsToCreate, (err) => {
+      if (err) {console.log(`There's an error on insert --> `, err)}
+    })
   }
+  console.timeEnd('WriteFilesToMongo')
 }
 
 let recordCount = 1000;
-let fileCount = 1;
+let fileCount = 1000;
 // writeToTxt(recordCount, fileCount)
-readFromTxt(fileCount);
+// readFromTxt(1);
+insertToMongo(0, 1000)
+// readFromTxt(10, 19);
+// readFromTxt(20, 29);
+// readFromTxt(30, 39);
+// readFromTxt(40, 49);
+// readFromTxt(50, 59);
+// readFromTxt(60, 69);
+// readFromTxt(70, 79);
+// readFromTxt(80, 89);
+// readFromTxt(90, 99);
