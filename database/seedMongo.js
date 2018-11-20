62b301f706b6517494da291016e68cc5c998d99a
Stephen Weiss <stephencweiss@gmail.com>
2018-11-20 15:43:23 -0800

    Mongo successfully seeded with 10m records; slower than PG
    Though we use bulkWrite to insert batches of 10,000, it appears to be an order of magnitude slower than Postgres.
    To insert 10m documents takes ~1hr.
    
    Still, this solution does *not* rely on writing to/reading from disk, which in a space constrained environment like the cloud will be considered a victory.

--- database/seedMongo.js
+++ database/seedMongo.js
@@ -1,23 +1,52 @@
+const chalk = require('chalk');
 const db = require('./index.js');
 const Description  = require('./db.js');
-const generateRandomDescription = require ('./generateSampleRecords.js')
+const { generateRandomDescription } = require ('./generateSampleRecords.js')
 
 // ---------- To Do ---------- //
-// 1. Refactor to have batch size and total record count (a la seedPg.js)
-// 2. Refactor to do bulkWrite (not .create) -- check Mongoose documentation for appropriate format
+// [x] 1. Refactor to have batch size and total record count (a la seedPg.js)
+// [x] 2. Refactor to do bulkWrite (not .create) -- check Mongoose documentation for appropriate format
 
-const createMongo = (recordsToCreate) => {
-  return Description.create(recordsToCreate)
+// ------------------ Query Builder ------------------ //
+const buildQuery = (recordstoCreate) => {
+  return recordstoCreate.map((record) => {return {insertOne: {document : record}}})
 }
 
-const insertToMongo = async function (fileStart, fileEnd) {
-  console.time('WriteFilesToMongo')
-  for (let i = fileStart; i < fileEnd; i++) {
-    let recordsToCreate = await readFromTxt(i)
-    await createMongo(recordsToCreate);
+// ------------------ Insert X Records Into Mongo ------------------ //
+const insertMongoRecords = async function (idStart, idEnd) {
+  return new Promise ((resolve, reject) => {
+    console.time('createABatch')
+    let generatedRecords = [];
+    for (let i = idStart; i <= idEnd; i++) {
+      generatedRecords.push(generateRandomDescription(i));
+    }
+    resolve(Description.bulkWrite(buildQuery(generatedRecords))
+      .then(() => {return console.timeEnd('createABatch')})
+      .catch((err) => console.error(chalk.red(`There was an error! -->`), err)))
+  })
+}
+
+// ------------------ Create A Batch Of Records ------------------ //
+const createAMongoBatch = async (totalRecordCount, batchCount) => {
+  console.time('BatchRun')
+  let batchSize = Math.floor(totalRecordCount / batchCount);
+  let start = 1;
+  let end = batchSize;
+  for (let i = 0; i < batchCount; i++) {
+    console.log(`The start value is ${start}, and end is ${end}`)
+    await insertMongoRecords(start, end);
+    start = end + 1;
+    end += batchSize;
   }
-  console.timeEnd('WriteFilesToMongo')
+  console.timeEnd('BatchRun');
+  console.log(`The BatchRun added ${totalRecordCount} records in ${batchCount} batches.`)
   db.close();
 }
 
-module.exports.insertToMongo = insertToMongo; 
+//Example Use:
+// createAMongoBatch(10000000, 1000)
+// Adds 10m records to Mongo in 1000 batches of 10,000;
+
+createAMongoBatch(1000000, 100);
+
+module.exports.createAMongoBatch = createAMongoBatch; 
