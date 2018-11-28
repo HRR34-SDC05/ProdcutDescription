892faac19e0ede93735e6c8ffddcbb2370848ece
Stephen Weiss <stephencweiss@gmail.com>
2018-11-28 12:24:05 -0800

    Added Table Create for PG in comments;
    While I haven't yet automated the creation / dropping of the table, adding the schema makes it clearer what the table should actually look like.

--- database/describePg.js
+++ database/describePg.js
@@ -2,3 +2,13 @@ const table = 'descriptions';
 const fields = 'product_id, product_name, features, tech_specs'
 module.exports.table = table;
 module.exports.fields = fields;
+
+// SQL statement to create table in Postgres
+// CREATE TABLE descriptions (
+//   product_id INTEGER NOT NULL,
+//   product_name VARCHAR (255),
+//   features TEXT[],
+//   tech_specs JSONB,
+//   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+//   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+// );
