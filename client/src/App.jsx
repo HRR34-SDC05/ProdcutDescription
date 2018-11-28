da2bd47dc0632946333556b30d0d4b1d32d1a156
Stephen Weiss <stephencweiss@gmail.com>
2018-11-28 12:25:36 -0800

    Streamline API for App.jsx and syntax clean up
    Removed cruft for API; added a baseURL instance

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,29 +21,13 @@ class Productdescriptions extends React.Component {
 
   componentDidMount(){
     let id = window.location.pathname.replace(/\/product\//,'');
-    //console.log(id);
-    //if(id){
-      // $.get('/productdescriptions',(data) =>{ //get all the data
-      //   console.log(data[0]);
-      //   this.setState({
-      //     product: data[0]
-      //   })
-      // })
-    // }else{
-      axios.create({baseURL:`http://localhost:${port}`})
-        axios
-        .get(`/product/data/` + id)
-        //.then(res => res.json())
-        .then(productData =>{
-          var productData = productData.data;
-          this.setState({
-            product: productData
-          })
-        })
-        .catch(err => {
-          console.log("Error with server side fetch request",err);
-        })
-    //}
+    let instance = axios.create({baseURL:`http://localhost:${port}`})
+    instance
+      .get(`/product/data/` + id)
+      .then(productData => {
+        this.setState({ product: productData.data[0] })
+      })
+      .catch(err => { console.log("Error with server side fetch request",err); })
   }
   changeView(){
     if(this.state.view === "Features"){
@@ -59,10 +43,10 @@ class Productdescriptions extends React.Component {
 
   renderView() {
     const {view} = this.state;
-    if(view === "Features"){
+    if(view === "Features") {
       return <Features features={this.state.product.features}/>
-    }else if (view === "TechSpecs"){
-      return <TechSpecs techSpecs={this.state.product.techSpecs}/>
+    }else if (view === "TechSpecs") {
+      return <TechSpecs techSpecs={this.state.product.tech_specs}/>
     }
   }
   render() {
