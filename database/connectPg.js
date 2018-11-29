21fd3df6395d90d6c16382591f6443d965550d43
Stephen Weiss <stephencweiss@gmail.com>
2018-11-29 14:15:25 -0800

    Moved environment flag to config file
    By setting the environment in the configuration file, we now have the capability to set both the database and server hosts in one place -- DRY.

--- database/connectPg.js
+++ database/connectPg.js
@@ -1,13 +1,13 @@
 const { Client } = require('pg');
 const config = require('../config.json')
 
-const environment = "production" // options are 'development' or 'production'
+const environment = config.currentEnvironment;
 
-const host = config[environment].host;
-const user = config[environment].username;
-const pw = config[environment].password;
-const db = config[environment].database;
-const port = config[environment].port;
+const host = config[environment].db_host;
+const user = config[environment].db_username;
+const pw = config[environment].db_password;
+const db = config[environment].db_database;
+const port = config[environment].db_port;
 const conString = `postgres://${user}:${pw}@${host}:${port}/${db}`;
 
 const client = new Client({
